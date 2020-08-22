e63408c4f58b996ca9fa6122d5070df808387df9
Ron Egli <ron@r-egli.com>
2020-08-22 00:07:01 -0700

    enhancing build script

--- build.sh
+++ build.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
-version=1.0.0
-docker build -t smugzombie/epicreminder:$version .
-docker run -td --restart unless-stopped smugzombie/epicreminder:$version 
+version=1.0.2
+tag=smugzombie/epicreminder
+name=epicreminder
+
+# Check if running
+running=$(docker ps | grep "$name" | awk {'print $1'})
+
+echo $running
+
+if [[ ! -z "$running" ]]
+then
+
+        echo "Running Instance Found.. Stopping"
+        docker stop $running
+        echo "Deleting Image"
+        docker rm $running
+
+fi
+
+# Build the image
+docker build -t $tag:$version .
+
+# Run the image
+docker run -td --restart unless-stopped --name $name $tag:$version
 
